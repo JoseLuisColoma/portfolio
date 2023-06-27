e4382123272c0438eae49ebea0804c2c2e0b46cc
JoseLuisColomaTormo <colomatormojl@gmail.com>
2023-06-27 10:57:10 +0200

    feat(#18): Se ha mejorado las parte del portfolio y se ha incluido una ventana de información para secciones en mantenimiento

--- script.js
+++ script.js
@@ -23,11 +23,11 @@ function seleccionar(){
 }
 
 //Descargar CV
-let archivoDescargado = false;
+let archivoEstaDescargado = false;
 
 function descargarCV() {
 // Verificar si el archivo ya ha sido descargado
-    if (archivoDescargado) {
+    if (archivoEstaDescargado) {
         // El archivo ya ha sido descargado, mostrar el aviso
         alert('El archivo ya ha sido descargado.\nSolo puedes descargar el CV una vez.');
         return;
@@ -41,7 +41,53 @@ function descargarCV() {
     document.body.appendChild(enlace);
     enlace.click();
     document.body.removeChild(enlace);
-
-    // Marco el archivo como descargado
-    archivoDescargado = true;
+    
+    archivoEstaDescargado = true; // Marco el archivo como descargado
 }
+
+function enMantenimiento() {
+    let overlay = createOverlay();
+    let popup = createPopup();
+    let closeButton = createCloseButton();
+    let content = createContent();
+  
+    popup.appendChild(content);
+    popup.appendChild(closeButton);
+    overlay.appendChild(popup);
+    document.body.appendChild(overlay);
+  
+    closeButton.addEventListener("click", function () {
+      document.body.removeChild(overlay);
+    });
+  
+    setTimeout(function () {
+      document.body.removeChild(overlay);
+    }, 3000);
+  }
+  
+  function createOverlay() {
+    let overlay = document.createElement("div");
+    overlay.className = "c-overlay";
+    return overlay;
+  }
+  
+  function createPopup() {
+    let popup = document.createElement("div");
+    popup.className = "c-popup";
+    return popup;
+  }
+  
+  function createCloseButton() {
+    let closeButton = document.createElement("button");
+    closeButton.innerHTML = "Aceptar";
+    closeButton.className = "c-popup__close-button";
+    return closeButton;
+  }
+  
+  function createContent() {
+    let content = document.createElement("p");
+    content.innerHTML = "Sección en mantenimiento.<br>Disculpa las molestias.";
+    content.className = "c-popup__content";
+    return content;
+  }
+  
